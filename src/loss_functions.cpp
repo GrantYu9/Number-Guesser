5cdde636d00d057b35af745c02fe0233eba39c21
Grant Yu <grantyu2019@gmail.com>
2026-09-12 18:15:40 -0700

    Test and implement loss functions

--- src/loss_functions.cpp
+++ src/loss_functions.cpp
@@ -1,13 +1,23 @@
+#include "globals.hpp"
 #include "loss_functions.hpp"
 
 #include <eigen3/Eigen/Core>
 
+namespace {
+    /** @brief A buffer to avoid ln(0) if the neural network completely
+     * incorrectly guesses the number of the image. */
+    constexpr float BUFFER = 1e-37f;
+}
+
 float cross_entropy_loss_function_error(
     const Eigen::MatrixXf& probabilities,
     const Eigen::MatrixXf& targets) {
-    // !!!
+    const Eigen::ArrayXXf probabilities_filtered = probabilities.array().max(
+        BUFFER);
+    const int batch_size = static_cast<int>(targets.rows());
 
-    return 0.0f;
+    return -(targets.array() == 1.0f).select(probabilities_filtered, 1.0f).
+        log().sum() / batch_size;
 }
 
 /** @brief Produces error gradients relative to the expected value.
@@ -18,7 +28,6 @@ float cross_entropy_loss_function_error(
 Eigen::MatrixXf cross_entropy_loss_gradients(
     const Eigen::MatrixXf& probabilities,
     const Eigen::MatrixXf& targets) {
-    // !!!
-
-    return Eigen::MatrixXf::Random();
+    Eigen::MatrixXf intermediate = probabilities - targets;
+    return intermediate / static_cast<float>(Globals::BATCH_SIZE);
 }
--- testing/test_loss_functions.cpp
+++ testing/test_loss_functions.cpp
@@ -3,6 +3,84 @@
 #include <eigen3/Eigen/Core>
 #include <gtest/gtest.h>
 
-// !!! cross entropy error
+#include <cmath>
+#include <tuple>
 
-// !!! cross entropy gradients
+namespace {
+    constexpr float TOLERANCE = 1e-5f;
+}
+
+Eigen::MatrixXf create_target_matrix();
+
+// !!! cross entropy error
+class TestCrossEntropyError : public testing::TestWithParam<std::tuple<const float, const Eigen::MatrixXf, const Eigen::MatrixXf>> {};
+
+TEST_P(TestCrossEntropyError, TestCrossEntropyError) {
+    const auto& [expected, probabilities, targets] = GetParam();
+
+    EXPECT_NEAR(expected, cross_entropy_loss_function_error(probabilities, targets), TOLERANCE);
+};
+
+INSTANTIATE_TEST_SUITE_P(TestCrossEntropyError, TestCrossEntropyError,
+    testing::Values(
+        // No error
+        std::tuple{
+            0.0f,
+            (Eigen::MatrixXf(2, 2) << 0.0f, 1.0f, 1.0f, 0.0f).finished(),
+            create_target_matrix()
+        },
+        // Some error
+        std::tuple{
+            -std::log(0.5f),
+            (Eigen::MatrixXf(2, 2) << 0.5f, 0.5f, 0.5f, 0.5f).finished(),
+            create_target_matrix()
+        },
+        // Avoiding ln(0)
+        std::tuple{
+            -std::log(1e-37f),
+            (Eigen::MatrixXf(2, 2) << 1.0f, 0.0f, 0.0f, 1.0f).finished(),
+            create_target_matrix()
+        },
+        // Some right, some wrong
+        std::tuple{
+            -(std::log(0.25f) + std::log(0.75f)) / 2.0f,
+            (Eigen::MatrixXf(2, 2) << 0.75f, 0.75f, 0.25f, 0.25f).finished(),
+            create_target_matrix()
+        }
+    )
+);
+
+class TestCrossEntropyGradients : public testing::TestWithParam<std::tuple<const Eigen::MatrixXf, const Eigen::MatrixXf, const Eigen::MatrixXf>> {};
+
+TEST_P(TestCrossEntropyGradients, TestCrossEntropyGradients) {
+    const auto& [expected, probabilities, targets] = GetParam();
+
+    EXPECT_EQ(expected, cross_entropy_loss_gradients(probabilities, targets));
+};
+
+INSTANTIATE_TEST_SUITE_P(TestCrossEntropyGradients, TestCrossEntropyGradients,
+    testing::Values(
+        // No gradient
+        std::tuple{
+            (Eigen::MatrixXf(2, 2) << 0.0f, 0.0f, 0.0f, 0.0f).finished(),
+            (Eigen::MatrixXf(2, 2) << 0.0f, 1.0f, 1.0f, 0.0f).finished(),
+            create_target_matrix()
+        },
+        // Test scaling
+        std::tuple{
+            (Eigen::MatrixXf(2, 2) << -1.0f, 1.0f, 1.0f, -1.0f).finished(),
+            (Eigen::MatrixXf(2, 2) << -32.0f, 33.0f, 33.0f, -32.0f).finished(),
+            create_target_matrix()
+        },
+        // Floats
+        std::tuple{
+            (Eigen::MatrixXf(2, 2) << -0.25f, 0.5f, 0.5f, -1.0f / 64.0f).finished(),
+            (Eigen::MatrixXf(2, 2) << -8.0f, 17.0f, 17.0f, -0.5f).finished(),
+            create_target_matrix()
+        }
+    )
+);
+
+Eigen::MatrixXf create_target_matrix() {
+    return (Eigen::MatrixXf(2, 2) << 0.0f, 1.0f, 1.0f, 0.0f).finished();
+}
